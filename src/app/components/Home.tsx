947cdee97111d2ce5e942ccb8c36e568f8a0116c
SauloAC <saulo.andrade.costa@gmail.com>
2025-01-13 02:07:24 -0800

    Home

---
+++ src/app/components/Home.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section id="home" className="py-10">
+      <div className="text-center">
+        <Image
+          src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-WWxszqUDsiFEvKsT0X3zdYTf9e6ouN.png"
+          alt="Saulo Costa"
+          width={400}
+          height={400}
+          className="mx-auto"
+        />
+      </div>
+      <h2 className="text-3xl font-bold text-center mt-8"></h2>
+      <h3 className="mt-4 text-lg text-justify">
+        I&apos;m a full-stack developer student, and my journey in web
+        development has been defined by a fervent passion for crafting visually
+        stunning and seamlessly functional websites. Driven by an insatiable
+        thirst for knowledge, I approach each project as an opportunity to
+        expand my skill set and tackle new challenges head-on.
+        <br />
+        <br />
+        Here you can see a little demonstration of what I&apos;m capable of
+        doing.
+      </h3>
+    </section>
+  );
+}
