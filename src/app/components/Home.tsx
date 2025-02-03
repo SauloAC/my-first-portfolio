5ddbc835b982d53b8f4184f74bccf41abc61699a
SauloAC <saulo.andrade.costa@gmail.com>
2025-02-03 03:03:57 -0800

    Implement Contact Form

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -15,11 +15,11 @@ export default function Home() {
       </div>
       <h2 className="text-3xl font-bold text-center mt-8"></h2>
       <h3 className="mt-4 text-lg text-justify">
-        I&apos;m a full-stack developer student, and my journey in web
-        development has been defined by a fervent passion for crafting visually
-        stunning and seamlessly functional websites. Driven by an insatiable
-        thirst for knowledge, I approach each project as an opportunity to
-        expand my skill set and tackle new challenges head-on.
+        I&apos;m a full-stack developer, and my journey in web development has
+        been defined by a fervent passion for crafting visually stunning and
+        seamlessly functional websites. Driven by an insatiable thirst for
+        knowledge, I approach each project as an opportunity to expand my skill
+        set and tackle new challenges head-on.
         <br />
         <br />
         Here you can see a little demonstration of what I&apos;m capable of
